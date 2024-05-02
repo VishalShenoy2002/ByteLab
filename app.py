5e2f61ae04e9b287d69140c2baf28889a8f8fd9d
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2024-05-02 09:33:17 +0530

    the main file which runs the flask application

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask
+from flask import render_template
+from flask import request, redirect
+from flask import jsonify
+
+from questions_module import Question
+ 
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/editor")
+def editor():
+    
+    return render_template("editor.html",title="ByteLab | Editor",language="python",question="C Question")
+
+
+@app.route("/")
+def index():
+    return redirect("/login")
+
+@app.route('/save',methods=["POST"])
+def saveCode():
+    code = request.form['code']
+    language = request.form['language']
+
+    # print(output)
+    return 'Code saved'
+
+@app.route("/login",methods=["GET","POST"])
+def login_page():
+    return render_template("login_page.html",title="ByteLab | Login")
+
+@app.route("/register",methods=["GET","POST"])
+def registration_page():
+    if request.method == "GET":
+        return render_template("registration_page.html",title="ByteLab | Registration")
+    
+    elif request.method == "POST":
+        return redirect("/login")
+    
+@app.route('/faculty-dashboard',methods=["GET","POST"])
+def faculty_dashboard_page():
+    if request.method == "GET":
+        return render_template("faculty-dashboard.html",title="ByteLab | Faculty Dashboard",page_hero_title="Faculty Dashboard")
+    
+
+@app.route('/manage-questions',methods=["GET"])
+def manage_questions_page():
+    if request.method == "GET":
+        return render_template("manage_questions.html",title="ByteLab | Manage Questions",page_hero_title="Manage Questions")
+    
+    
+@app.route('/manage-questions/add',methods=["GET","POST"])
+def add_questions_page():
+    if request.method == "GET":
+        return render_template("add_questions.html",title="Manage Questions | Add",page_hero_title="Add Question")
+    
+    elif request.method == "POST":
+        form_data = request.form
+        record = {}
+        record['question'] = form_data.get('question')
+        record['semester'] = int(form_data.get('semester'))
+        record['programming_language'] = form_data.get('programming_language')
+        
+        del form_data
+        question = Question(record['question'],record['semester'],record['programming_language'],"")
+        question.insert_into_questions_table()
+
+        return render_template("add_questions.html",title="Manage Questions | Add",page_hero_title="Add Question")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
+    
+    
