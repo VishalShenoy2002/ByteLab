44aa57e1adf616240ebcf59c19b507daf2e3f391
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2024-05-08 23:47:18 +0530

    added courses module to handle courses

---
+++ course_module.py
@@ -0,0 +1,25 @@
+import mysql.connector as mysql
+
+conn = mysql.connect(host="localhost",user="root",passwd="asdasd123",database="bytelab")
+cursor = conn.cursor()
+
+class Course:
+    
+    def __init__(self) -> None:
+        self.course_name: str = None
+        self.full_form: str = None
+        self.no_of_sems: int = None
+        
+    def insert_into_table(self):
+        
+        query=f'INSERT INTO courses(course_name,full_form,num_of_sems)VALUES("{self.course_name}","{self.full_form}",{self.no_of_sems});'
+        cursor.execute(query)
+        conn.commit()
+        
+    def list_courses(self):
+        query = "SELECT course_name FROM courses;"
+        cursor.execute(query)
+        
+        records = cursor.fetchall()
+        records = [x[0] for x in records]
+        return records
